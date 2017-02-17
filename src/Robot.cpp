2e33fc600ab72ab76c5df2874fd67e724f1a4039
Kira <kira.e.noel@gmail.com>
2017-02-17 16:45:41 -0500

    Removed non-working vision thread

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,38 +7,6 @@ std::shared_ptr<Gear> Robot::gear;
 std::shared_ptr<Shooter> Robot::shooter;
 std::unique_ptr<OI> Robot::oi;
 
-void Robot::VisionThread() {
-	// Get the USB camera from CameraServer
-		cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();
-		// Set the resolution
-		camera.SetResolution(640, 480);
-
-		// Get a CvSink. This will capture Mats from the Camera
-		cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
-		// Setup a CvSource. This will send images back to the Dashboard
-		cs::CvSource outputStream = CameraServer::GetInstance()->
-				PutVideo("Rectangle", 640, 480);
-
-		// Mats are very memory expensive. Let's reuse this Mat.
-		cv::Mat mat;
-
-		while (true) {
-			// Tell the CvSink to grab a frame from the camera and put it
-			// in the source mat.  If there is an error notify the output.
-			if (cvSink.GrabFrame(mat) == 0) {
-				// Send the output the error.
-				outputStream.NotifyError(cvSink.GetError());
-				// skip the rest of the current iteration
-				continue;
-			}
-			// Put a rectangle on the image
-			rectangle(mat, cv::Point(100, 100), cv::Point(400, 400),
-					cv::Scalar(255, 255, 255), 5);
-			// Give the output stream a new image to display
-			outputStream.PutFrame(mat);
-		}
-}
-
 void Robot::RobotInit() {
 	RobotMap::init();
 
